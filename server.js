712393a1562fa439c14ad35b3aa7c027ae76e03b
phact <estevezsebastian@gmail.com>
2021-11-11 08:30:52 -0500

    updates

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const AWS = require("aws-sdk");
 const fs = require('fs');
+const readline = require('readline');
+
 
 const args = process.argv;   
 
@@ -10,14 +12,37 @@ if (args.length < 3){
 console.log(args[2])
 
 filePath = args[2]
+
+/*
 fs.readFile(filePath , 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
   //console.log(data)
-  parsed  = AWS.DynamoDB.Converter.unmarshall(JSON.parse(data))
+  jsonData = JSON.parse(data)
+  parsed  = AWS.DynamoDB.Converter.unmarshall(jsonData)
   console.log(parsed)
 
 })
+*/
 
+async function processLineByLine() {
+
+    const fileStream = fs.createReadStream(filePath);
+
+    const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+    });
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+
+    for await (const line of rl) {
+      jsonData = JSON.parse(line)
+      parsed  = AWS.DynamoDB.Converter.unmarshall(jsonData)
+      console.log(JSON.stringify(parsed))
+    }
+}
+
+processLineByLine();
